019f6d03f5f62e8e42909013cddeaefb62d4b1e4
jw-02-pfda <johnywakim02@gmail.com>
2025-08-03 11:36:50 +0300

    started setting up hive

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,26 @@
-import 'package:flutter/material.dart';
-import 'package:strive_flow/core/navigation/go_router_routes.dart';
+import 'dart:io' as io;
 
-void main() {
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:hive_ce_flutter/adapters.dart';
+import 'package:strive_flow/core/navigation/go_router_routes.dart';
+import 'package:path_provider/path_provider.dart' as path_provider;
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  if (!kIsWeb && (io.Platform.isAndroid || io.Platform.isIOS)) {
+    final dir = await path_provider.getApplicationDocumentsDirectory();
+    Hive.init(dir.path);
+  } else {
+    Hive.initFlutter(); // Works for web and desktop
+  }
+
+  var box = await Hive.openBox('testBox'); // open a box and get the instance to it
+  await box.put('test', 'test_data'); // store a value
+  var name = box.get('test'); // retrieve a value
+  debugPrint(name);
+
   runApp(const MyApp());
 }
 
