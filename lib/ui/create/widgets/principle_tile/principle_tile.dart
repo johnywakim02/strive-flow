c543e71314418fd7b6c690b5d86af250fc566d7a
jw-02-pfda <johnywakim02@gmail.com>
2025-08-14 22:17:15 +0300

    added the ability to expand the principle tile and contract it

--- lib/ui/create/widgets/principle_tile/principle_tile.dart
+++ lib/ui/create/widgets/principle_tile/principle_tile.dart
@@ -5,7 +5,7 @@ import 'package:strive_flow/ui/create/widgets/principle_tile/principle_tile_red_
 import 'package:strive_flow/ui/create/widgets/principle_tile/principle_tile_red_line.dart';
 import 'package:strive_flow/ui/create/widgets/principle_tile/principle_tile_title_text.dart';
 
-class PrincipleTile extends StatelessWidget{
+class PrincipleTile extends StatefulWidget{
   final String title;
   final String description;
 
@@ -16,49 +16,67 @@ class PrincipleTile extends StatelessWidget{
   });
 
   @override
+  State<PrincipleTile> createState() => _PrincipleTileState();
+}
+
+class _PrincipleTileState extends State<PrincipleTile> {
+  bool _isExpanded = false;
+
+  void _toggleExpanded() {
+    setState(() {
+      _isExpanded = !_isExpanded;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(12), 
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: [AppColors.lightButterCream, AppColors.softGoldenPeach], 
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-        ),
-        borderRadius: BorderRadius.circular(12),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black12,
-            blurRadius: 6,
-            offset: Offset(0, 2),
+    return GestureDetector(
+      onTap: _toggleExpanded,
+      child: Container(
+        padding: EdgeInsets.all(12), 
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [AppColors.lightButterCream, AppColors.softGoldenPeach], 
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
           ),
-        ],
-      ),
-      child: Align(
-        alignment: Alignment.centerLeft, // to align text to the left in the column (part 1)
-        child: IntrinsicHeight(
-          child: Row(
-            spacing: 12.0,
-            crossAxisAlignment: CrossAxisAlignment.start, // to align text to the left in the column (part 2)
-            children: [
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start, 
-                children: [
-                  PrincipleTileRedDisk(),
-                  Expanded(child: PrincipleTileRedLine()),
-                ],
-              ),
-              Expanded(
-                child: Column(
-                  spacing:12.0,
+          borderRadius: BorderRadius.circular(12),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black12,
+              blurRadius: 6,
+              offset: Offset(0, 2),
+            ),
+          ],
+        ),
+        child: Align(
+          alignment: Alignment.centerLeft, // to align text to the left in the column (part 1)
+          child: IntrinsicHeight(
+            child: Row(
+              spacing: 12.0,
+              crossAxisAlignment: CrossAxisAlignment.start, // to align text to the left in the column (part 2)
+              children: [
+                Column(
                   crossAxisAlignment: CrossAxisAlignment.start, 
                   children: [
-                    PrincipleTileTitleText(title: title), 
-                    PrincipleTileDescriptionText(description: description), 
+                    PrincipleTileRedDisk(),
+                    if(_isExpanded)
+                      Expanded(child: PrincipleTileRedLine()),
                   ],
                 ),
-              ),
-            ],
+                Expanded(
+                  child: Column(
+                    spacing:12.0,
+                    crossAxisAlignment: CrossAxisAlignment.start, 
+                    children: [
+                      PrincipleTileTitleText(title: widget.title), 
+                      if(_isExpanded)
+                        PrincipleTileDescriptionText(description: widget.description), 
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
